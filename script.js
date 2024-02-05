e1f5a67f7bc2e945532e18bfb890ef8e817c58cc
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-05 23:34:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const student = {
+	name:"jhon"
+}
+
+student.prototype.getKeys = function(){
+	return Object.keys(this);
+}
+
+var keys = student.getKeys();
+
+// Log the keys to the console
+// alert("Keys:", keys);
